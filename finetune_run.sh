edf33621f7387af0ca3be712eddac22a6fcfe980
Prashant Limba <108218976+prashant852@users.noreply.github.com>
2024-06-11 12:36:00 +0530

    Update finetune_run.sh

--- finetune_run.sh
+++ finetune_run.sh
@@ -1,8 +1,8 @@
 python train.py \
 --train_csv "/home/TEXT-CLASSIFICATION/notebooks/data/comp/train.csv" \
 --label2id "/home/TEXT-CLASSIFICATION/config/label2id.csv" \
---fold_file "/home/TEXT-CLASSIFICATION/data/PERSUADE-LB-EVENTRATE-SEED-8.csv" \
---base_model "/home/TEXT-CLASSIFICATION/notebooks/data/datasets" \
+--fold_file "/home/TEXT-CLASSIFICATION/data/PERSUADE-SEED-8.csv" \
+--base_model "microsoft/deberta-v3-large" \
 --base_tokenizer "microsoft/deberta-v3-large" \
 --num_labels 6 \
 --fold_strategy "oof-csv" \
@@ -19,9 +19,9 @@ python train.py \
 --epochs 2 \
 --batch_size 1 \
 --eval_batch_size 1 \
---grad_acc_steps 8 \
---learning_rate 3.5e-5 \
---lr_scheduler "linear" \
+--grad_acc_steps 4 \
+--learning_rate 5e-5 \
+--lr_scheduler "cosine" \
 --warmup_ratio 0.1 \
 --freeze_layers -1 \
 --freeze_embeddings "False" \
@@ -32,4 +32,4 @@ python train.py \
 --eval_delay 1000 \
 --eval_steps 100 \
 --report_to "wandb" \
---experiment "EX040-SEED-8" 
+--experiment "Pretrain-Experiment-1" 
